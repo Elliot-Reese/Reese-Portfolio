adb315b0bc6743a4610fff7916a7890397225d62
Elliot-Reese <egreese@wpi.edu>
2025-03-20 12:40:32 -0400

    eb site updated

--- src/app/projects/abb-internship/page.tsx
+++ src/app/projects/abb-internship/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       <div className="max-w-4xl mx-auto text-center mt-4">
         <h1 className="text-5xl font-bold text-gray-900 dark:text-white">ABB Internship</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6">
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center mt-2 mb-2">
               <Image
                 src="/images/ABB-logo.png"
                 alt="ABB Logot"
@@ -43,7 +43,7 @@ export default function Home() {
             <p className="text-lg text-gray-700 dark:text-gray-300 mt-6">
               Inititally, I was tasked with designing fixtures to aid in the assembly process of these breakers, but as the team grew to understand my strengths, I was asked to join an effort to create an integrated method of padlocking the breaker in the off position for maitanance, something that had never been accomplished on a molded case circuit breaker.
             </p>
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center mt-2 mb-2">
               <Image
                 src="/images/XT1.jpg"
                 alt="XT1 Circuit Breaker"
@@ -52,7 +52,7 @@ export default function Home() {
                 className="rounded-lg shadow-lg"
               />
             </div>
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center mt-2 mb-2">
               <Image
                 src="/images/breaker-loto.webp"
                 alt="Breaker Lock-Out-Tag-Out Mechanism"
--- src/app/projects/eb-internship/page.tsx
+++ src/app/projects/eb-internship/page.tsx
@@ -28,35 +28,52 @@ export default function Home() {
       <div className="max-w-4xl mx-auto text-center mt-4">
         <h1 className="text-5xl font-bold text-gray-900 dark:text-white">General Dynamics Electric Boat Internship</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6">
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center mt-4 mb-4">
               <Image
                 src="/images/EB-sub.webp"
                 alt="Nuclear Sub"
-                width={400}
+                width={300}
                 height={300}
                 className="rounded-lg shadow-lg"
               />
             </div>
             <div className="flex items-center justify-center">
               <p className="text-lg text-gray-700 dark:text-gray-300 mt-6">
-                The summer of 2023, I joined the robotics engineering division of General Dynamics Electric Boat.
+                The summer of 2023, I joined the robotics engineering division of General Dynamics Electric Boat. The team was responsible for identifying applications of robotics that would expedite and improve the construction of nuclear submarines.
               </p>
               </div>
             <div className="flex items-center justify-center">
               <p className="text-lg text-gray-700 dark:text-gray-300 mt-6">
-                My first task was to compare the usability and reliability of several different cobots (collaborative robots), then to develop a training program for the most promising model.
+                My first task was to compare the usability and reliability of several different cobots (collaborative robots), then to develop a training program for the most promising model. This required an in depth understanding of the intricacies of the systems in order to explain how to use the systems effectively to people with no prior experience with robots or cobots.
               </p>
             </div>
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center mt-4 mb-4">
               <Image
                 src="/images/fanuc-cobot.jpg"
                 alt="Fanuc Cobot"
-                width={400}
+                width={300}
                 height={300}
                 className="rounded-lg shadow-lg"
               />
             </div>
+            <div className="flex items-center justify-center mt-4 mb-4">
+              <Image
+                src="/images/Quad-pack.webp"
+                alt="Common Missile Compartment"
+                width={300}
+                height={300}
+                className="rounded-lg shadow-lg"
+              />
+            </div>
+            <div className="flex items-center justify-center">
+              <p className="text-lg text-gray-700 dark:text-gray-300">
+                Once my training modules had been implemented, I worked on modeling and designing a new facility layout that could incorporate robotic systems into the production of components for the new Colombia class submarine. 
+                </p>
+            </div>
           </div>
+          <p className="text-m text-gray-700 dark:text-gray-300 mt-6 mb-12">
+              *Actual facility and models not shown for security reasons.
+          </p>
         
       </div>
     </div>
